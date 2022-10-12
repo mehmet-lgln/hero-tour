9bfcccd9753f84e81f84eab09ccb557162b7b50c
mehmet-lgln <mehmet.karakaya@lgln.niedersachsen.de>
2022-10-12 17:06:18 +0200

    created mock, displayed heroes and selected hero

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { Heroes } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -11,6 +12,14 @@ export class HeroesComponent implements OnInit {
   hero: Hero = {
     id: 1,
     name: "First Hero"
+  }
+
+  heroes = Heroes;
+
+  selectedHero?: Hero
+  onSelect(h: Hero): void {
+    this.selectedHero = h;
+    console.log(this.selectedHero)
   }
 
   constructor() { }
