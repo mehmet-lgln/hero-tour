3b615fadc8a3ad2505b424dcc3c4d75e8bca6100
mehmet-lgln <mehmet.karakaya@lgln.niedersachsen.de>
2022-10-17 17:12:06 +0200

    added navigation to hero detail

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { HeroesComponent } from './heroes/heroes.component';
 
 const routes: Routes = [
   { path: '', component: DashboardComponent },
-  { path: 'heroes', component: HeroesComponent }
+  { path: 'heroes', component: HeroesComponent },
+  { path: 'hero/:id', component: HeroDetailComponent }
 ];
 
 @NgModule({
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -11,22 +11,22 @@ import { MessageService } from '../message.service';
 })
 export class HeroesComponent implements OnInit {
 
-  hero: Hero = {
-    id: 1,
-    name: "First Hero"
-  }
+  // hero: Hero = {
+  //   id: 1,
+  //   name: "First Hero"
+  // }
 
   heroes: Hero[] = [];
 
-  selectedHero?: Hero
-  onSelect(h: Hero): void {
-    this.selectedHero = h;
-    console.log(this.selectedHero)
-    this.messageService.add(`selected hero: ${h.name}`)
-    console.log(this.messageService.message)
-  }
+  // selectedHero?: Hero
+  // onSelect(h: Hero): void {
+  //   this.selectedHero = h;
+  //   console.log(this.selectedHero)
+  //   this.messageService.add(`selected hero: ${h.name}`)
+  //   console.log(this.messageService.message)
+  // }
 
-  constructor(private heroService: HeroService, private messageService: MessageService) { }
+  constructor(private heroService: HeroService) { }
 
   getHeroes(){
     this.heroService.getHeroes().subscribe(h => this.heroes = h)
